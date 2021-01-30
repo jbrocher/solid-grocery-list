8e4f8501d01499fd0122cc7481803b5d7f0e45ed
Jean-Baptiste Rocher <jeanb.rocher@gmail.com>
2021-01-30 22:31:18 +0100

    Install Formik, React-Router and Storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,14 @@
+import React from "react";
+import theme from "theme";
+import { ThemeProvider } from "styled-components";
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <Story />
+    </ThemeProvider>
+  ),
+];
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+};
