014d3b1fa6721fa7cfdd981f0466ff7bbe260597
jb <jeanb.rocher@gmail.com>
2021-04-03 19:59:03 +0200

    Add fo ontology constants

--- src/models/iris.ts
+++ src/models/iris.ts
@@ -1,2 +1,7 @@
 export const FOOD = "http://purl.org/ontology/fo/Food";
 export const SHOPPING_CATEGORY = "http://purl.org/ontology/fo/ShoppingCategory";
+export const INGREDIENT = "http://purl.org/ontology/fo/Ingredient";
+export const METRIC_QUANTITY = "http://purl.org/ontology/fo/metric_quantity";
+export const INGREDIENTS = "http://purl.org/ontology/fo/ingredients";
+export const RECIPE = "http://purl.org/ontology/fo/Recipe";
+export const INGREDIENTLIST = "http://purl.org/ontology/fo/IngredientList";
