1b08e5f0efd283b4865b14bf91cd6e7d5bed06e9
jb <jeanb.rocher@gmail.com>
2021-04-17 15:48:33 +0200

    Add measure utils

---
+++ src/utils/useMeasure.ts
@@ -0,0 +1,23 @@
+import { useRef, useState, useEffect } from "react";
+import ResizeObserver from "resize-observer-polyfill";
+
+export function useMeasure() {
+  const ref = useRef<HTMLUListElement>(null);
+  const [bounds, setBounds] = useState({
+    left: 0,
+    top: 0,
+    width: 0,
+    height: 0,
+  });
+  const [resizeObserver] = useState(
+    () => new ResizeObserver(([entry]) => setBounds(entry.contentRect))
+  );
+  useEffect(() => {
+    if (ref.current !== null) {
+      console.log(ref);
+      resizeObserver.observe(ref.current);
+    }
+    return () => resizeObserver.disconnect();
+  }, [resizeObserver]);
+  return { ref, bounds };
+}
