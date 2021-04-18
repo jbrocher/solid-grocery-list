f033f2411964f2c056a98450b689e9c92cfeebaf
jb <jeanb.rocher@gmail.com>
2021-04-18 10:23:24 +0200

    Add 100vh utils

---
+++ src/utils/use100vh.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+export const use100vh = () => {
+  const [measuredWindowHeight, setMeasuredWindowHeight] = useState<
+    string | number
+  >(0);
+  useEffect(() => {
+    const measureHeight = () => {
+      return document.documentElement?.clientHeight || window.innerHeight;
+    };
+
+    const updateHeight = () => {
+      setMeasuredWindowHeight(measureHeight());
+    };
+    updateHeight();
+    window.addEventListener("resize", updateHeight);
+    return window.removeEventListener("resize", updateHeight);
+  }, []);
+  return measuredWindowHeight > 0 ? measuredWindowHeight : "100vh";
+};
