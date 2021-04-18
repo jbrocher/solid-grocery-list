2d6b7ade13a610a80d55df5c0a49808487a3959c
jb <jeanb.rocher@gmail.com>
2021-04-18 18:50:33 +0200

    Create useFoodList

--- src/utils/api/hooks/food.ts
+++ src/utils/api/hooks/food.ts
@@ -4,6 +4,8 @@ import { FOOD_NAME, FOOD, SHOPPING_CATEGORY } from "models/iris";
 import { TripleDocument } from "tripledoc";
 import { useEffect, useState } from "react";
 import { getFoods } from "utils/api/helpers";
+import { foodSerializer } from "utils/api/serializers";
+import { Food } from "utils/api/types";
 
 export const useFoods = () => {
   const { profile, publicTypeIndex } = useProfile();
@@ -14,6 +16,19 @@ export const useFoods = () => {
     }
   }, [profile, publicTypeIndex]);
   return foods;
+};
+
+export const useFoodList = () => {
+  const foods = useFoods();
+  const [foodItems, setFoodItems] = useState<Food[] | null>(null);
+  useEffect(() => {
+    if (foods) {
+      setFoodItems(
+        foods.getAllSubjectsOfType(FOOD).map((food) => foodSerializer(food))
+      );
+    }
+  }, [foods]);
+  return foodItems;
 };
 
 export const useCreateFood = () => {
