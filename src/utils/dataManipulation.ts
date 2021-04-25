95289ed1c1445bd54d855caa3f3abf8ef41f2685
jb <jeanb.rocher@gmail.com>
2021-04-25 11:28:18 +0200

    Create group by ingredient function

--- src/utils/dataManipulation.ts
+++ src/utils/dataManipulation.ts
@@ -1,1 +1,18 @@
-export {};
+import { Recipe, Ingredient } from "utils/api/types";
+
+type returnValue = {
+  [index: string]: number;
+};
+export const groupByIngredients = (recipes: Recipe[]) => {
+  const ingredients = recipes.reduce(
+    (result, value) => result.concat(value.ingredients),
+    [] as Ingredient[]
+  );
+
+  return ingredients.reduce((result, ingredient) => {
+    result[ingredient.food.identifier] = result[ingredient.food.identifier]
+      ? result[ingredient.food.identifier] + ingredient.quantity
+      : ingredient.quantity;
+    return result;
+  }, {} as returnValue);
+};
