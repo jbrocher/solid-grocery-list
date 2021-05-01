94e763ec8e96640c90a0e67b579a7827d09881d5
jb <jeanb.rocher@gmail.com>
2021-05-01 18:28:51 +0200

    Create groupbyshopping category utils

--- src/utils/dataManipulation.ts
+++ src/utils/dataManipulation.ts
@@ -1,4 +1,5 @@
 import { Recipe, Ingredient } from "utils/api/types";
+import { GroceryListItem, GroceryList } from "utils/api/types";
 
 type returnValue = {
   [index: string]: number;
@@ -15,4 +16,22 @@ export const groupByIngredients = (recipes: Recipe[]) => {
       : ingredient.quantity;
     return result;
   }, {} as returnValue);
+};
+
+type GroupedByCategory = {
+  [index: string]: GroceryListItem[];
+};
+
+export const groupByShoppingCategory = (groceryList: GroceryList) => {
+  return groceryList.items.reduce(
+    (result: GroupedByCategory, ingredient: GroceryListItem) => {
+      if (result[ingredient.object.category]) {
+        result[ingredient.object.category].push(ingredient);
+      } else {
+        result[ingredient.object.category] = [];
+      }
+      return result;
+    },
+    {} as GroupedByCategory
+  );
 };
