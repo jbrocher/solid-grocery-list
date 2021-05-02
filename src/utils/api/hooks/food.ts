1da1cc56f25137182862a025d90812018ec08ddc
jb <jeanb.rocher@gmail.com>
2021-05-02 16:15:07 +0200

    Use food model in hooks

--- src/utils/api/hooks/food.ts
+++ src/utils/api/hooks/food.ts
@@ -2,16 +2,18 @@ import { useProfile } from "ProfileContext";
 import { useQuery } from "react-query";
 import { rdf } from "rdf-namespaces";
 import { FOOD_NAME, FOOD, SHOPPING_CATEGORY } from "models/iris";
-import { TripleSubject, TripleDocument } from "tripledoc";
 import { useState } from "react";
-import { getFoods } from "utils/api/helpers";
+import FoodManager from "models/Food";
 import { foodSerializer } from "utils/api/serializers";
 
 export const useFoods = () => {
   const { profile, publicTypeIndex } = useProfile();
   const { data: foods } = useQuery(
     ["food", profile, publicTypeIndex],
-    () => getFoods(profile as TripleSubject, publicTypeIndex as TripleDocument),
+    async () => {
+      const manager = new FoodManager(profile!, publicTypeIndex!);
+      return await manager.getFoods();
+    },
     { enabled: !!profile && !!publicTypeIndex }
   );
   return foods;
@@ -33,8 +35,6 @@ export const useCreateFood = () => {
     if (!foods) {
       throw new Error("Missing FoodList");
     }
-    console.log(name);
-    console.log(foods.asRef());
 
     const food = foods.addSubject();
     food.addRef(rdf.type, FOOD);
