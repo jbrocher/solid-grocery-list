96c415a73e13a65df530441b6b658ca6c4c694b1
jb <jeanb.rocher@gmail.com>
2021-05-16 17:42:21 +0200

    Use LinkButton component in groceries

--- src/pages/Groceries/Groceries.tsx
+++ src/pages/Groceries/Groceries.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useGroceryLists } from "utils/api/hooks/groceryLists";
-import { Link } from "react-router-dom";
+import LinkButton from "components/atoms/LinkButton";
 import GoBackHeader from "components/atoms/GoBackHeader";
 import Loading from "pages/Loading";
 import Page from "components/templates/Page";
@@ -16,7 +16,10 @@ const Groceries: React.FunctionComponent = () => {
       <GoBackHeader title="Groceries" />
       {groceryLists.map((list) => (
         <div key={list.identifier}>
-          <Link to={`/groceries/${list.identifier}`}> {list.title} </Link>
+          <LinkButton
+            link={`/groceries/${list.identifier}`}
+            title={list.title}
+          />
         </div>
       ))}
     </Page>
