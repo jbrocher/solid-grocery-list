5fde0e7f244b06fcc8b647fe8f9c72eda6cfa0a9
Jean-Baptiste Rocher <jeanb.rocher@gmail.com>
2021-11-05 17:38:38 +0100

    refacto(Resource): Use solid client in resource

--- src/models/Resource.ts
+++ src/models/Resource.ts
@@ -1,10 +1,18 @@
 import { rdf, solid, space } from "rdf-namespaces";
+
 import {
-  createDocument,
-  fetchDocument,
-  TripleSubject,
-  TripleDocument,
-} from "tripledoc";
+  createSolidDataset,
+  createThing,
+  buildThing,
+  saveSolidDatasetAt,
+  setThing,
+  getSolidDataset,
+  getThingAll,
+  getUrl,
+  Thing,
+  SolidDataset,
+} from "@inrupt/solid-client";
+import { fetch } from "@inrupt/solid-client-authn-browser";
 
 interface Resource {
   identifier: string;
@@ -12,13 +20,13 @@ interface Resource {
   iri: string;
 }
 export class ResourceManager {
-  profile: TripleSubject;
-  publicTypeIndex: TripleDocument;
+  profile: Thing;
+  publicTypeIndex: SolidDataset;
   resource: Resource;
 
   constructor(
-    profile: TripleSubject,
-    publicTypeIndex: TripleDocument,
+    profile: Thing,
+    publicTypeIndex: SolidDataset,
     resource: Resource
   ) {
     this.profile = profile;
@@ -26,42 +34,67 @@ export class ResourceManager {
     this.publicTypeIndex = publicTypeIndex;
   }
   makeRef(identifier: string) {
-    const storage = this.profile.getRef(space.storage);
+    const storage = getUrl(this.profile, space.storage);
     // Decide at what URL within the user's Pod the new Document should be stored:
     return `${storage}${this.resource.storage}#${identifier}`;
   }
 
-  async createRessource(): Promise<TripleDocument> {
-    const storage = this.profile.getRef(space.storage);
-
-    // Decide at what URL within the user's Pod the new Document should be stored:
-    const ref = storage + this.resource.storage;
-    // Create the new Document:
-    const list = createDocument(ref);
-    list.save();
-
-    // Store a reference to that Document in the public Type Index for `schema:TextDigitalDocument`:
-    const typeRegistration = this.publicTypeIndex.addSubject({
-      identifier: this.resource.identifier,
-    });
-    typeRegistration.addRef(rdf.type, solid.TypeRegistration);
-    typeRegistration.addRef(solid.instance, list.asRef());
-    typeRegistration.addRef(solid.forClass, this.resource.iri);
-    return await this.publicTypeIndex.save([typeRegistration]);
+  getBaseUrl() {
+    const storage = getUrl(this.profile, space.storage);
+    return `${storage}${this.resource.storage}`;
   }
 
-  async getOrCreate(): Promise<TripleDocument> {
-    const entry = this.publicTypeIndex.findSubject(
-      solid.forClass,
-      this.resource.iri
+  async createRessource(): Promise<SolidDataset> {
+    const storage = getUrl(this.profile, space.storage);
+
+    // Decide at what URL within the user's Pod the new Dataset should be stored:
+    const ref = storage + this.resource.storage;
+
+    // Create the new Dataset:
+    let list = createSolidDataset();
+    list = await saveSolidDatasetAt(ref, list, { fetch: fetch });
+
+    // Store a reference to that Document in the public Type Index for `schema:TextDigitalDocument`:
+    const typeRegistration = buildThing(
+      createThing({
+        name: this.resource.identifier,
+      })
+    )
+      .addUrl(rdf.type, solid.TypeRegistration)
+      .addUrl(solid.instance, ref)
+      .addUrl(solid.forClass, this.resource.iri)
+      .build();
+
+    let publicTypeIndex = await getSolidDataset(
+      getUrl(this.profile, solid.publicTypeIndex) as string
+    );
+    publicTypeIndex = setThing(publicTypeIndex, typeRegistration);
+    console.log(publicTypeIndex);
+
+    publicTypeIndex = await saveSolidDatasetAt(
+      getUrl(this.profile, solid.publicTypeIndex) as string,
+      publicTypeIndex,
+      { fetch: fetch }
+    );
+    return list;
+  }
+
+  async getOrCreate(): Promise<SolidDataset> {
+    const publicTypeIndex = await getSolidDataset(
+      getUrl(this.profile, solid.publicTypeIndex) as string
+    );
+    const entry = getThingAll(publicTypeIndex).find(
+      (thing) => getUrl(thing, solid.forClass) === this.resource.iri
     );
 
     if (entry == null) {
       return await this.createRessource();
     } else {
-      const ressourceRef = entry.getRef(solid.instance);
+      const ressourceRef = getUrl(entry, solid.instance);
       if (ressourceRef) {
-        const ressourceList = await fetchDocument(ressourceRef);
+        const ressourceList = await getSolidDataset(ressourceRef, {
+          fetch: fetch,
+        });
         return ressourceList;
       } else {
         throw new Error("Invalid recipe list type registry");
