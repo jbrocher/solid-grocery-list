6ce269dd296e881f4a1640c7b5556439b9c8dc78
Jean-Baptiste Rocher <jeanb.rocher@gmail.com>
2021-11-06 10:26:49 +0100

    refacto(recipe)

--- src/models/Recipe.ts
+++ src/models/Recipe.ts
@@ -3,13 +3,22 @@ import { rdf } from "rdf-namespaces";
 import IngredientManager from "./Ingredient";
 import FoodManager from "./Food";
 import { RecipeFormValues } from "pages/RecipeForm/RecipeForm";
-import { TripleSubject, TripleDocument } from "tripledoc";
+import {
+  asUrl,
+  Thing,
+  SolidDataset,
+  setThing,
+  createThing,
+  buildThing,
+  saveSolidDatasetAt,
+} from "@inrupt/solid-client";
+import { fetch } from "@inrupt/solid-client-authn-browser";
 import { INGREDIENT, RECIPE, TITLE } from "models/iris";
 
 class RecipeManager extends ResourceManager {
   ingredients: IngredientManager;
   foods: FoodManager;
-  constructor(profile: TripleSubject, publicTypeIndex: TripleDocument) {
+  constructor(profile: Thing, publicTypeIndex: SolidDataset) {
     super(profile, publicTypeIndex, {
       identifier: "recipe",
       storage: "public/recipe-list.ttl",
@@ -18,7 +27,7 @@ class RecipeManager extends ResourceManager {
     this.ingredients = new IngredientManager(profile, publicTypeIndex);
     this.foods = new FoodManager(profile, publicTypeIndex);
   }
-  async getRecipes(): Promise<TripleDocument> {
+  async getRecipes(): Promise<SolidDataset> {
     return this.getOrCreate();
   }
 
@@ -31,21 +40,29 @@ class RecipeManager extends ResourceManager {
   }
 
   async create(recipe: RecipeFormValues) {
-    const recipes = await this.getRecipes();
-    const recipeSubject = recipes.addSubject();
-    recipeSubject.addRef(rdf.type, RECIPE);
-    recipeSubject.addString(TITLE, recipe.title);
+    let recipes = await this.getRecipes();
+    const recipeSubjectBuilder = buildThing(createThing())
+      .addUrl(rdf.type, RECIPE)
+      .setStringNoLocale(TITLE, recipe.title);
     await Promise.all(
       recipe.ingredients.map(async (ingredient) => {
         const createdIngredient = await this.ingredients.create(ingredient);
-        recipeSubject.addRef(INGREDIENT, createdIngredient.asRef());
+        recipeSubjectBuilder.addUrl(
+          INGREDIENT,
+          asUrl(createdIngredient, this.ingredients.getBaseUrl())
+        );
         return createdIngredient;
       })
     );
-    await recipes.save();
+    const recipeSubject = recipeSubjectBuilder.build();
+    recipes = setThing(recipes, recipeSubject);
+    console.log(recipes);
+    recipes = await saveSolidDatasetAt(this.getBaseUrl(), recipes, {
+      fetch: fetch,
+    });
     return {
       ...recipe,
-      identifier: recipeSubject.asRef().split("#")[1],
+      identifier: asUrl(recipeSubject, this.getBaseUrl()).split("#")[1],
     };
   }
 }
