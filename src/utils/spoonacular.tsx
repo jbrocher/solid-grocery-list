815ca423c7c3f0859a0435d476afd059376a0365
Jean-Baptiste Rocher <jeanb.rocher@gmail.com>
2021-11-21 22:18:59 +0100

    feature(spoonacular): Fix autocomplete

--- src/utils/spoonacular.tsx
+++ src/utils/spoonacular.tsx
@@ -8,7 +8,10 @@ export interface SpoonacularIntgredient {
 
 export const useAutocompleteIngredient = (query: string) => {
   const { isSuccess, data } = useQuery<SpoonacularIntgredient[]>(
-    `https://api.spoonacular.com/food/ingredients/autocomplete?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&query=${query}&metaInformation=true`
+    `https://api.spoonacular.com/food/ingredients/autocomplete?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&query=${query}&metaInformation=true`,
+    {
+      keepPreviousData: true,
+    }
   );
 
   return { isSuccess, data };
