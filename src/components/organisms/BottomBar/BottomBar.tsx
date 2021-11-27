7414df4456b8a07872493e7d3c2a62d88072d217
Jean-Baptiste Rocher <jeanb.rocher@gmail.com>
2021-11-27 11:44:35 +0100

    feat(BottomBar): Style Bottom Bar

--- src/components/organisms/BottomBar/BottomBar.tsx
+++ src/components/organisms/BottomBar/BottomBar.tsx
@@ -1,51 +1,42 @@
 import React from "react";
-import BottomBarButton from "./components/BottomBarButton";
-import styled from "styled-components";
-import ShoppingCart from "components/icons/ShoppingCart";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { Link as RouterLink } from "react-router-dom";
 import Egg from "components/icons/Egg";
 import Recipe from "components/icons/Recipe";
-import HomeButton from "components/icons/HomeButton";
-import { useHistory, useLocation } from "react-router";
-
-const Container = styled.div`
-  width: 100%;
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const HomeButtonContainer = styled.div`
-  position: absolute;
-  border-radius: ${(props) => props.theme.radii[2]}px;
-  order: 2;
-  border: solid 2px;
-  border-color: ${(props) => props.theme.colors.mandarinRed};
-  padding: ${(props) => props.theme.space[1]}px;
-  width: ${(props) => props.theme.space[3]}px;
-  height: ${(props) => props.theme.space[3]}px;
-`;
+import {  useLocation } from "react-router";
 
 const BottomBar: React.FunctionComponent = () => {
-  const history = useHistory();
   const location = useLocation();
   if (location.pathname === "/") return null;
-  const goHome = () => history.push("/");
   return (
-    <Container>
-      <HomeButtonContainer onClick={goHome}>
-        <HomeButton />
-      </HomeButtonContainer>
-      <BottomBarButton to="/food-list" Icon={Egg}>
+    <BottomNavigation showLabels>
+      <BottomNavigationAction
+        label="foods"
+        component={RouterLink}
+        to="/food-list"
+        icon={<Egg />}
+      >
         Foods
-      </BottomBarButton>
-      <BottomBarButton to="/recipe-list" Icon={Recipe}>
+      </BottomNavigationAction>
+      <BottomNavigationAction
+        label="recipes"
+        component={RouterLink}
+        to="/recipe-list"
+        icon={<Recipe />}
+      >
         Recipes
-      </BottomBarButton>
-      <BottomBarButton to="/groceries" Icon={ShoppingCart}>
+      </BottomNavigationAction>
+      <BottomNavigationAction
+        label="groceries"
+        component={RouterLink}
+        to="/groceries"
+        icon={<ShoppingCartIcon />}
+      >
         Groceries
-      </BottomBarButton>
-    </Container>
+      </BottomNavigationAction>
+    </BottomNavigation>
   );
 };
 
