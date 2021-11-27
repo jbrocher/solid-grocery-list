a015363f22d75652154997fda554709dd69c9ca9
Jean-Baptiste Rocher <jeanb.rocher@gmail.com>
2021-11-27 12:42:13 +0100

    feat(ingredient_list):Style quantity

--- src/pages/RecipeList/components/RecipeDetail/IngredientList.tsx
+++ src/pages/RecipeList/components/RecipeDetail/IngredientList.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Collapse from "@mui/material/Collapse";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
+import Typography from "@mui/material/Typography";
 import { Ingredient } from "utils/api/types";
 
 interface IngredientListProps {
@@ -18,8 +19,18 @@ const IngredientList: React.FunctionComponent<IngredientListProps> = ({
       <List>
         {ingredients.map((ingredient) => {
           return (
-            <ListItem key={ingredient.identifier}>
-              {ingredient.food.name} - x{ingredient.quantity}
+            <ListItem
+              sx={{ justifyContent: "space-between" }}
+              key={ingredient.identifier}
+            >
+              {ingredient.food.name}
+              <Typography
+                color="text.secondary"
+                fontWeight="bolder"
+                variant="body2"
+              >
+                {ingredient.quantity}
+              </Typography>
             </ListItem>
           );
         })}
