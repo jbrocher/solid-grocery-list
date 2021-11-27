e0d5a32245df25435fe4e73483ba056d52746b06
Jean-Baptiste Rocher <jeanb.rocher@gmail.com>
2021-11-27 11:25:03 +0100

    feat(grocery_list): Style grocerly list

--- src/pages/Groceries/GroceryList/GroceryList.tsx
+++ src/pages/Groceries/GroceryList/GroceryList.tsx
@@ -9,7 +9,13 @@ import {
 } from "utils/api/hooks/groceryLists";
 import Text from "components/atoms/Text";
 import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemSecondaryAction from "@mui/material/ListItemButton";
+import Checkbox from "@mui/material/Checkbox";
 import Loading from "pages/Loading";
+import Typography from "@mui/material/Typography";
 
 type GroceryListParams = {
   identifier: string;
@@ -24,25 +30,39 @@ export const GroceryList: React.FunctionComponent = () => {
   }
   const list = groceryListQuery.data!;
   const groupedByCategory = groupByShoppingCategory(list);
-  console.log(groupedByCategory);
 
   const renderList = () => {
     return Object.keys(groupedByCategory).map((category) => {
       return (
         <Box key={category}>
-          <Text variant="h3"> {category} </Text>
-          {groupedByCategory[category].map((item) => {
-            return (
-              <li key={item.identifier}>
-                <input
-                  onChange={() => check.mutate(item)}
-                  type="checkbox"
-                  checked={item.done}
-                />
-                {`${item.object.name} -x${item.quantity}`}
-              </li>
-            );
-          })}
+          <Text variant="h6"> {category} </Text>
+          <List>
+            {groupedByCategory[category].map((item) => {
+              return (
+                <ListItemButton onClick={() => check.mutate(item)} dense>
+                  <ListItem
+                    sx={{
+                      justifyContent: "space-between",
+                    }}
+                    disablePadding
+                    key={item.identifier}
+                  >
+                    <Box>
+                      <Checkbox edge="start" checked={item.done} />
+                      {item.object.name}
+                    </Box>
+                    <Typography
+                      color="text.secondary"
+                      fontWeight="bolder"
+                      variant="body2"
+                    >
+                      {item.quantity}
+                    </Typography>
+                  </ListItem>
+                </ListItemButton>
+              );
+            })}
+          </List>
         </Box>
       );
     });
