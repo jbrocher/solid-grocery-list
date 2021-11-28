124733e2b4d365a74e2fe7e09374ea4fc3f4dacd
Jean-Baptiste Rocher <jeanb.rocher@gmail.com>
2021-11-28 15:02:49 +0100

    feat: Style recipe form

--- src/pages/RecipeForm/RecipeForm.tsx
+++ src/pages/RecipeForm/RecipeForm.tsx
@@ -3,10 +3,11 @@ import { useHistory } from "react-router";
 import { Formik, FormikProps, FieldArray } from "formik";
 import IngredientModal from "components/organisms/modals/IngredientModal";
 import Button from "@mui/material/Button";
-import Input from "components/atoms/Input";
-import Text from "components/atoms/Text";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
 import Page from "components/templates/Page";
-import Card from "@mui/material/Card";
+import GoBackHeader from "components/atoms/GoBackHeader";
+import { styled } from "@mui/material/styles";
 import { useCreateRecipe } from "utils/api/hooks/recipe";
 
 import { Ingredient } from "utils/api/types";
@@ -30,6 +31,11 @@ const initialValues = {
   title: "",
   ingredients: [],
 };
+const StyledFrom = styled("form")`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 const RecipeForm: React.FunctionComponent = () => {
   const { ready, recipeMutation } = useCreateRecipe();
   const [isIngredientModalOpen, setIsIngredientModalOpen] = useState(false);
@@ -58,64 +64,61 @@ const RecipeForm: React.FunctionComponent = () => {
         values,
         handleSubmit,
       }: FormikProps<RecipeFormValues>) => (
-        <Page>
-          <Card>
-            <Text textAlign="center" mb={2} variant="h1">
-              New recipe
-            </Text>
-            <form onSubmit={handleSubmit}>
-              <Input
-                value={values.title}
-                label="Recipe Name"
-                onChange={handleChange}
-                name="title"
-              />
-              <FieldArray
-                name="ingredients"
-                render={(arrayHelpers) => (
-                  <div>
-                    <Text mt={2} mb={1} variant="h3">
-                      Ingredients
-                    </Text>
-                    {values.ingredients.map((ingredient, index) => (
-                      <div key={index}>
-                        <Text variant="body1">
-                          {ingredient.food.name}: {ingredient.quantity}
-                        </Text>
-                      </div>
-                    ))}
-                    <IngredientModal
-                      isOpen={isIngredientModalOpen}
-                      toggle={toggleIngredientModal}
-                      handleSubmit={(ingredient) => {
-                        arrayHelpers.push(ingredient);
-                        toggleIngredientModal();
-                      }}
-                    />
-                    <Button
-                      sx={{
-                        marginY: 2,
-                      }}
-                      variant="outlined"
-                      type="button"
-                      onClick={toggleIngredientModal}
-                    >
-                      Add ingredient
-                    </Button>
-                  </div>
-                )}
-              />
-              <Button
-                sx={{
-                  marginY: 2,
-                }}
-                disabled={!isValid || isSubmitting || !ready}
-                type="submit"
-              >
-                Create recipe
-              </Button>
-            </form>
-          </Card>
+        <Page sx={{ alignItems: "center" }}>
+          <GoBackHeader title="New recipe" />
+          <StyledFrom onSubmit={handleSubmit}>
+            <TextField
+              value={values.title}
+              label="Recipe Name"
+              onChange={handleChange}
+              name="title"
+            />
+            <FieldArray
+              name="ingredients"
+              render={(arrayHelpers) => (
+                <div>
+                  <Typography textAlign="center" mt={2} mb={1} variant="h6">
+                    Ingredients
+                  </Typography>
+                  {values.ingredients.map((ingredient, index) => (
+                    <div key={index}>
+                      <Typography variant="body1">
+                        {ingredient.food.name}: {ingredient.quantity}
+                      </Typography>
+                    </div>
+                  ))}
+                  <IngredientModal
+                    isOpen={isIngredientModalOpen}
+                    toggle={toggleIngredientModal}
+                    handleSubmit={(ingredient) => {
+                      arrayHelpers.push(ingredient);
+                      toggleIngredientModal();
+                    }}
+                  />
+                  <Button
+                    sx={{
+                      marginY: 2,
+                    }}
+                    variant="outlined"
+                    type="button"
+                    onClick={toggleIngredientModal}
+                  >
+                    Add ingredient
+                  </Button>
+                </div>
+              )}
+            />
+            <Button
+              sx={{
+                marginY: 2,
+              }}
+              disabled={!isValid || isSubmitting || !ready}
+              variant="contained"
+              type="submit"
+            >
+              Create recipe
+            </Button>
+          </StyledFrom>
         </Page>
       )}
     </Formik>
