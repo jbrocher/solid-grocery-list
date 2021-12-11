41c10546e525e3b872a3bfcc08f10d2dceae0698
Jean-Baptiste Rocher <jeanb.rocher@gmail.com>
2021-12-11 19:57:13 +0100

    feat(groceries): Add creation form to groceries list

--- src/pages/Groceries/Groceries.tsx
+++ src/pages/Groceries/Groceries.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import theme from "theme";
+
+import React, { useState } from "react";
 
 import { Link as RouterLink } from "react-router-dom";
 
 import { useGroceryLists } from "utils/api/hooks/groceryLists";
 
+import AddIcon from "@mui/icons-material/Add";
+import Fab from "@mui/material/Fab";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
@@ -12,10 +16,12 @@ import Typography from "@mui/material/Typography";
 import Loading from "pages/Loading";
 
 import GoBackHeader from "components/atoms/GoBackHeader";
+import GroceriesListCreation from "components/organisms/GroceriesListCreation";
 import Page from "components/templates/Page";
 
 const Groceries: React.FunctionComponent = () => {
   const { isSuccess, groceryLists } = useGroceryLists();
+  const [openForm, setOpenForm] = useState(false);
   if (!isSuccess) {
     return <Loading />;
   }
@@ -39,6 +45,20 @@ const Groceries: React.FunctionComponent = () => {
           </ListItem>
         ))}
       </List>
+      <Fab
+        sx={{
+          position: "absolute",
+          bottom: theme.spacing(8),
+          right: theme.spacing(2),
+        }}
+        color="primary"
+      >
+        <AddIcon onClick={() => setOpenForm(true)} />
+      </Fab>
+      <GroceriesListCreation
+        onClose={() => setOpenForm(false)}
+        open={openForm}
+      />
     </Page>
   );
 };
