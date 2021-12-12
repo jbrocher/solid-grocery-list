10d23d5f7e63d6ef3629c0e1b39a2e4619974f60
Jean-Baptiste Rocher <jeanb.rocher@gmail.com>
2021-12-12 18:49:07 +0100

    fix: Content

--- src/components/templates/Page/Content.tsx
+++ src/components/templates/Page/Content.tsx
@@ -4,7 +4,14 @@ import Box from "@mui/material/Box";
 
 export const Content: React.FunctionComponent = ({ children }) => {
   return (
-    <Box overflow="auto" flexShrink={1} flexGrow={1}>
+    <Box
+      width="100%"
+      padding={2}
+      overflow="auto"
+      minHeight={0}
+      flexShrink={1}
+      flexGrow={1}
+    >
       {children}
     </Box>
   );
