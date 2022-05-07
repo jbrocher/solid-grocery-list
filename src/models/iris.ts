e9aef9f406ebad007415c7a3b135619e64e21786
Jean-Baptiste Rocher <jeanb.rocher@gmail.com>
2022-05-07 18:36:49 +0200

    Remove useless iri

--- src/models/iris.ts
+++ src/models/iris.ts
@@ -13,6 +13,5 @@ export const SHOPPING_CATEGORY = "http://purl.org/ontology/fo/ShoppingCategory";
 export const METRIC_QUANTITY = "http://purl.org/ontology/fo/metric_quantity";
 export const QUANTITY = "http://purl.org/ontology/fo/quantity";
 export const INGREDIENTS = "http://purl.org/ontology/fo/ingredients";
-export const groceryListItemName = "http://gl/GroceryListItemName";
 export const groceryListItemDone = "http://gl/GroceryListItemDone";
 export const groceryListItemObject = "http://gl/GroceryListItemObject";
